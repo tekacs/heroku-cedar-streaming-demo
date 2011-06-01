708ac1dbfb71f146ba5b1e0bafa36a1fe098aef7
Amar Sood <mail@tekacs.com>
2011-06-01 12:50:41 +0100

    Updated stream.rb.

--- stream.rb
+++ stream.rb
@@ -9,8 +9,11 @@ class Stream < Goliath::API
   
   def response(env)
     
-    keepalive = EM.add_periodic_timer(1) do
-      env.stream_send(".\n")
+    # This timer keeps the connection alive later in the stream when
+    # the number generation slows down sufficiently for > 30s response time.
+    # Yes, the timer is something like 55s but I like 30s, okay? ;)
+    keepalive = EM.add_periodic_timer(29) do
+      env.stream_send("Heartbeat.\n")
     end
     
     EM.defer do
@@ -23,7 +26,8 @@ class Stream < Goliath::API
       end
     end
     
-    # EM.add_timer(10) do
+    # The below cuts off the connection at some point if this is desired.
+    # EM.add_timer(30) do
     #   keepalive.cancel
     #   
     #   env.stream_send("End of stream.")
